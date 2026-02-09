426a99a1fab00c6f28797667248c8e3d8d62df36
Sainath <sainath.velaga@gmail.com>
2026-02-09 08:40:47 +0530

    project

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -2,3 +2,8 @@ variable "project_name" {
   description = "Project name"
   type        = string
 }
+
+variable "bucket_name" {
+  description = "S3 bucket name for Lambda trigger"
+  type        = string
+}
